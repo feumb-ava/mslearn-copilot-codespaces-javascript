46ab13de2958d5472d06fa644729ebaf9f2af8f9
Fernanda Umberto <f.pereira.umberto@avanade.com>
2025-02-21 13:53:02 +0000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Fernanda Umberto Portfolio",
+  title: "Full-stack Developer",
+  email: "fernanda.umberto@hotmail.com",
+  gitHub: "fe-umb",
+  instagram: "fe_umb",
+  linkedIn: "fernanda-umberto",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "sailornanda",
+  youTube: "sailornanda",
 };
 
 const primaryColor = "#4E567E";
